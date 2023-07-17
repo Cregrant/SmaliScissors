8e2e8eafa6e9b5ac959309f023b08851cbeef92d
Cregrant <cregrant90@gmail.com>
2023-07-18 05:10:41 +0700

    test: increase memory requirements for every compress thread

--- patcher/src/test/java/com/github/cregrant/smaliscissors/functional/Utils/Concurrent.java
+++ patcher/src/test/java/com/github/cregrant/smaliscissors/functional/Utils/Concurrent.java
@@ -10,8 +10,8 @@ public class Concurrent {
     public static final ExecutorService WORKER = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
     //start a long-running background task
     public static final ExecutorService LONG_WORKER = Executors.newCachedThreadPool();
-    public static final Semaphore compressSemaphore = new Semaphore(getThreadsByMemory(600));
-    public static final Semaphore runPatcherSemaphore = new Semaphore(getThreadsByMemory(600));
+    public static final Semaphore compressSemaphore = new Semaphore(getThreadsByMemory(800));
+    public static final Semaphore runPatcherSemaphore = new Semaphore(getThreadsByMemory(800));
 
     public static int getThreadsByMemory(long megabytesPerThread) {
         return Math.max(1, (int) (Runtime.getRuntime().maxMemory() / megabytesPerThread * 1024 * 1024));
